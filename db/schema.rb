913ab29c598dad3a7e6b3c4ed2c14c3fcab1f5f0
gtroppee <gtroppee@gmail.com>
2015-10-23 14:35:10 +0200

    add name to phone numbers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023121529) do
+ActiveRecord::Schema.define(version: 20151023122135) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20151023121529) do
     t.integer  "callable_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
