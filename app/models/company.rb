6d1747bd89f84ceaafdda26f0073606312361728
gtroppee <gtroppee@gmail.com>
2015-10-23 15:43:21 +0200

    WIP

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,9 @@
 class Company < ActiveRecord::Base
+  has_many :users, dependent: :destroy
   has_many :phone_numbers, as: :callable,
                            dependent: :destroy
+
+  def to_s
+    name
+  end
 end
