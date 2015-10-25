c7fabd0a45a9cfbf6d17b0af338a1937fd10c033
gtroppee <gtroppee@gmail.com>
2015-10-25 20:56:40 +0100

    wip

--- app/models/call_dispatcher.rb
+++ app/models/call_dispatcher.rb
@@ -18,7 +18,6 @@ class CallDispatcher
                           .find_by(sip_endpoint: @call.recipient_sip)
       number.users.where.not(id: forwarded_user_ids)
                   .joins(:phone_number_assignments)
-                  .order('phone_number_assignments.priority ASC')
                   .distinct
     end
 
--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,9 +1,10 @@
 class PhoneNumber < ActiveRecord::Base
   has_many :phone_number_assignments, dependent: :destroy
-  
+
   has_many :users, through: :phone_number_assignments,
                    source: :callable,
-                   source_type: 'User'
+                   source_type: 'User',
+                   order: "phone_number_assignments.priority ASC"
 
   has_many :companies, through: :phone_number_assignments,
                        source: :callable,
