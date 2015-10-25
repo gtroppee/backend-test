9be9a52b3f13a5e6a1b49ee8f17f114317d8f029
gtroppee <gtroppee@gmail.com>
2015-10-25 21:07:25 +0100

    wip

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,8 +1,10 @@
 class PhoneNumber < ActiveRecord::Base
   has_many :phone_number_assignments, dependent: :destroy
 
-  has_many :users, -> { order 'phone_number_assignments.priority ASC' },
-                   -> { select 'DISTINCT users.*, phone_number_assignments.priority' },
+  has_many :users, -> { 
+                      order 'phone_number_assignments.priority ASC';
+                      select 'DISTINCT users.*, phone_number_assignments.priority'
+                    },
                    through: :phone_number_assignments,
                    source: :callable,
                    source_type: 'User'
