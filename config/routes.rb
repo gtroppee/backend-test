54cb3ddbfc92aa33d8de6d02a783d837dc8f9c5a
gtroppee <gtroppee@gmail.com>
2015-10-26 18:06:35 +0100

    refactor calls routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'companies#index'
-  resources :calls, only: :index
+  resources :calls, only: :index do
+    collection do
+      post :dial
+      post :voicemail
+      post :hangup
+    end
+  end
   resources :companies, only: :index
-  match '/calls/:action' => 'calls#action', via: [:post, :get]
 end
